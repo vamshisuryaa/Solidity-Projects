16bfd23f8d99cf5163812344fe8766b1cb5fa9bc
vamshisuryaa <vamshi.suryapally@gmail.com>
2021-11-27 14:54:48 +0530

    commit

--- ERC20_Token_Smart_Contract/token.sol
+++ ERC20_Token_Smart_Contract/token.sol
@@ -46,7 +46,7 @@ contract Cryptos is ERC20Interface{
        return balances[tokenOwner]; 
     }
     
-     function transfer(address to, uint tokens) public override returns (bool success){
+     function transfer(address to, uint tokens) public virtual override returns (bool success){
         require(balances[msg.sender]>tokens,"NOT ENOUGH TOKENS");
         balances[to]+=tokens;
         balances[msg.sender]-=tokens;
@@ -70,7 +70,7 @@ contract Cryptos is ERC20Interface{
           return true;
       }
       
-       function transferFrom(address from, address to, uint tokens) public override returns (bool success){
+       function transferFrom(address from, address to, uint tokens) public virtual override returns (bool success){
            require(tokens<= allowed[from][to]);
            require(balances[from]>=tokens);
            balances[from] -=tokens;
@@ -80,9 +80,111 @@ contract Cryptos is ERC20Interface{
            return true;
            
        }
+}
+
+
+contract CryptosICO  is Cryptos{
     
-   
-   
+    address public admin;
+    address payable public deposit;
+    uint tokenPrice = 0.001 ether; // 1eth == 1000 CRPT, 1CRPT = 0.001ETH
+    uint public hardCap = 300 ether;
+    uint public raisedAmount;// in wei
+    uint public saleStart = block.timestamp;//+3600 for ICO to start in 1hour
+    uint public saleEnd = block.timestamp + 604800; // ICO ends in one weeks
+    uint public tokenTradeStart = saleEnd + 604800; //transferable in a week after sale end
+    uint public maxInvestment = 5 ether;
+    uint public minInvestment = 0.1 ether;
+    
+    enum State {beforeStart, Running, afterEnd, halted}
+    
+    State  public icoState; 
+    
+    
+    constructor(address payable _deposit){
+        
+        deposit = _deposit;
+        admin = msg.sender;
+        icoState = State.beforeStart;
+    }
+    
+    
+    modifier onlyAdmin(){
+        require(admin ==msg.sender,"YOU ARENT THE ADMIN");
+        _;
+    }
+    
+    function halt() public onlyAdmin{
+        icoState = State.halted;
+    }
+    
+    function resume() public onlyAdmin{
+        icoState= State.Running;
+    }
+    
+    function changeDepositAddress(address payable _newDepositAddress)public onlyAdmin{
+        deposit = _newDepositAddress;
+    }
+    
+    function getCurrentState() public view returns(State){
+        if(icoState == State.halted){
+            return State.halted;
+        } 
+        else if(block.timestamp < saleStart) {
+            return State.beforeStart;
+        }
+        else if(block.timestamp >= saleStart && block.timestamp <=saleEnd){
+            return State.Running;
+        }
+        
+        else{
+            return State.afterEnd;
+        }
+    }
+    
+    
+    event Invest(address investor, uint value, uint tokens);
+    function invest() payable public returns(bool){
+        require(getCurrentState() == State.Running);
+        require(msg.value >= minInvestment && msg.value <= maxInvestment);
+        raisedAmount += msg.value;
+        require(raisedAmount <= hardCap);
+        
+        uint tokens = msg.value/tokenPrice;
+        
+        balances[msg.sender] += tokens;
+        balances[founder] -= tokens;
+        
+        deposit.transfer(msg.value);
+        
+        emit Invest(msg.sender, msg.value,tokens);
+    
+        return true;
+    }
+    
+    receive() payable external{
+        invest();
+    }
+        
+     function transfer(address to, uint tokens) public override returns (bool success){
+      require(block.timestamp > tokenTradeStart);
+      Cryptos.transfer(to,tokens);// super.transfer(to,tokens); -> or we can use keyword super
+      return true;
+     }
+     
+     
+      function transferFrom(address from, address to, uint tokens) public override returns (bool success){
+      require(block.timestamp > tokenTradeStart);
+      Cryptos.transferFrom(from,to,tokens);  return true;
+      }
+      
+      function burn() public returns(bool){
+          icoState = getCurrentState();
+          require(icoState==State.afterEnd);
+          balances[founder]=0;
+          
+          return true;
+      }
     
     
 }
